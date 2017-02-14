c68d8f538881bc452914435b0ed6da89cffae76c
Damandeep Singh <singh.damandeep@gmail.com>
2017-02-14 11:09:33 +0530

    Initial Push

---
+++ WoocommerceClient/WoocommerceClient.h
@@ -0,0 +1,19 @@
+//
+//  WoocommerceClient.h
+//  WoocommerceClient
+//
+//  Created by Damandeep Singh on 14/02/17.
+//  Copyright © 2017 Damandeep Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WoocommerceClient.
+FOUNDATION_EXPORT double WoocommerceClientVersionNumber;
+
+//! Project version string for WoocommerceClient.
+FOUNDATION_EXPORT const unsigned char WoocommerceClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WoocommerceClient/PublicHeader.h>
+
+
